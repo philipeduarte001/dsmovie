e095ea81b2845850981428e223eb3bf965fcc928
Luiz Philipe <philipeduarte001@gmail.com>
2022-01-13 17:39:17 -0300

    Domain model, database seed

--- backend/src/test/java/com/projetogithub/dsmovie/DsmovieApplicationTests.java
+++ backend/src/test/java/com/projetogithub/dsmovie/DsmovieApplicationTests.java
@@ -1,7 +1,8 @@
 package com.projetogithub.dsmovie;
 
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+		import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DsmovieApplicationTests {
